0d6aed4b7066764b511e1550ad470b0818539304
lscleriston <lscleriston@gmail.com>
2024-12-20 11:56:32 -0300

    Ajuste no caminho para o arquivo run.py 2.xlsx

--- flask-web-app/run.py
+++ flask-web-app/run.py
@@ -1,10 +1,15 @@
+import sys
+import os
+
+# Adicionar o diretório pai ao sys.path para que o módulo 'app' possa ser encontrado
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 """
 from app import create_app
